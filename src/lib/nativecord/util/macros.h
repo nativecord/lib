94533165065e2945919ecd6668d8864007c76397
neru <152752583+neeeruuu@users.noreply.github.com>
2024-04-26 16:37:13 -0300

    fix: add exports on non shared builds

--- src/lib/nativecord/util/macros.h
+++ src/lib/nativecord/util/macros.h
@@ -3,18 +3,14 @@
 /*
     compiler specific macros
 */
-#ifdef NATIVECORD_LIB_SHARED
-    #if defined(_WIN32) || defined(__CYGWIN__)
-        #ifdef __GNUC__
-            #define NC_EXPORT __attribute__((dllexport))
-        #else
-            #define NC_EXPORT __declspec(dllexport)
-        #endif
-    #elif defined(__GNUC__) && __GNUC__ >= 4
-        #define NC_EXPORT __attribute__((visibility("default")))
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef __GNUC__
+        #define NC_EXPORT __attribute__((dllexport))
     #else
-        #define NC_EXPORT
+        #define NC_EXPORT __declspec(dllexport)
     #endif
+#elif defined(__GNUC__) && __GNUC__ >= 4
+    #define NC_EXPORT __attribute__((visibility("default")))
 #else
     #define NC_EXPORT
 #endif
