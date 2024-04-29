90b9dbc0a534100bd3d264d0e543e778440b437c
neru <152752583+neeeruuu@users.noreply.github.com>
2024-04-29 02:49:51 -0300

    feat: json std::conditional support

--- src/lib/nativecord/util/jsonutils.h
+++ src/lib/nativecord/util/jsonutils.h
@@ -50,3 +50,59 @@
 
 // clang-format on
 
+/*
+    std::contional compatibility
+    inspired by:
+        https://www.kdab.com/jsonify-with-nlohmann-json/
+*/
+template <typename> constexpr bool isOptional = false;
+template <typename T> constexpr bool isOptional<std::optional<T>> = true;
+
+template <class T, class J> inline void optionalToJson(J& j, const char* name, const std::optional<T>& value)
+{
+    if (value)
+        j[name] = *value;
+}
+
+template <class T, class J> inline void toJson(const char* key, J& j, const T& value)
+{
+    if constexpr (isOptional<T>)
+        optionalToJson(j, key, value);
+    else
+        j[key] = value;
+}
+
+template <class T, class J> inline void optionalFromJson(J& j, const char* name, std::optional<T>& value)
+{
+    const auto it = j.find(name);
+    if (it != j.end())
+        value = it->get<T>();
+    else
+        value = std::nullopt;
+}
+
+template <typename T, class J> inline void fromJson(const char* key, const J& j, T& value)
+{
+    if (!j.contains(key))
+        return;
+
+    if (j.at(key).is_null())
+        return;
+
+    if constexpr (isOptional<T>)
+        optionalFromJson(j, key, value);
+    else
+        j.at(key).get_to(value);
+}
+
+#define NC_JSON_TO_WITH_OPTIONAL(val) toJson(#val, nlohmann_json_j, nlohmann_json_t.val);
+#define NC_JSON_FROM_WITH_OPTIONAL(val) fromJson(#val, nlohmann_json_j, nlohmann_json_t.val);
+
+#define NC_JSON_DECLFUNCS(Type, ...)                                                                                   \
+    NC_EXPORT inline void to_json(nlohmann::json& nlohmann_json_j, const Type& nlohmann_json_t){                       \
+        NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(NC_JSON_TO_WITH_OPTIONAL, __VA_ARGS__))}                              \
+                                                                                                                       \
+    NC_EXPORT inline void from_json(const nlohmann::json& nlohmann_json_j, Type& nlohmann_json_t)                      \
+    {                                                                                                                  \
+        NLOHMANN_JSON_EXPAND(NLOHMANN_JSON_PASTE(NC_JSON_FROM_WITH_OPTIONAL, __VA_ARGS__));                            \
+    }
