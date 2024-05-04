03bef625ba6a4bc5fae9a7ef6c9f302a6cc72fc8
neru <152752583+neeeruuu@users.noreply.github.com>
2024-05-03 21:26:23 -0300

    style: move endpoints locatino

---
+++ src/lib/nativecord/discord/endpoints.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "util/macros.h"
+
+#define NC_DISCORD "https://discord.com"
+
+#define NC_API_VERSION 9
+#define NC_API_URL NC_DISCORD "/api/v" STR(NC_API_VERSION) "/"
+#define NC_WSS_URL "wss://gateway.discord.gg/?encoding=json&v=" STR(NC_API_VERSION)
--- src/lib/nativecord/endpoints.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "util/macros.h"
-
-#define NC_DISCORD "https://discord.com"
-
-#define NC_API_VERSION 9
-#define NC_API_URL STR(NC_DISCORD) "/api/v" STR(NC_API_VERSION) "/"
-#define NC_WSS_URL "wss://gateway.discord.gg/?encoding=json&v=" STR(NC_API_VERSION)
