dbaf1205eb0ca190032a3227c7981dd427d6a5b5
neru <152752583+neeeruuu@users.noreply.github.com>
2024-05-03 21:16:47 -0300

    fix: simplify compiler macros

--- src/lib/nativecord/util/macros.h
+++ src/lib/nativecord/util/macros.h
@@ -9,36 +9,27 @@
 /*
     compiler specific macros
 */
-#if defined(_WIN32) || defined(__CYGWIN__)
-    #ifdef __GNUC__
-        #define NC_EXPORT __attribute__((dllexport))
-    #else
-        #define NC_EXPORT __declspec(dllexport)
-    #endif
-#elif defined(__GNUC__) && __GNUC__ >= 4
-    #define NC_EXPORT __attribute__((visibility("default")))
-#else
-    #define NC_EXPORT
-#endif
-
-#ifdef __GNUC__
-    #define NC_NORET [[noreturn]]
-#elif defined _WIN32
-    #define NC_NORET __declspec(noreturn)
-#else
-    #define NC_NORET [[noreturn]]
-#endif
-
 #ifdef _MSC_VER
+    #define NC_EXPORT __declspec(dllexport)
+    #define NC_NORET __declspec(noreturn)
+
     #define NC_WARNING_PUSH __pragma(warning(push))
     #define NC_WARNING_POP __pragma(warning(pop))
     #define NC_DISABLE_WARNING(warningNumber) __pragma(warning(disable : warningNumber))
 #elif defined(__GNUC__)
+    #if __GNUC__ >= 4
+        #define NC_EXPORT __attribute__((visibility("default")))
+    #else
+        #define NC_EXPORT __attribute__((dllexport))
+    #endif
+    #define NC_NORET [[noreturn]]
+
     #define NC_WARNING_PUSH _Pragma("GCC diagnostic push")
     #define NC_WARNING_POP _Pragma("GCC diagnostic pop")
-    #define NC_DISABLE_WARNING(warningName)                                                                            \
-        _Pragma("GCC diagnostic ignored \"" STR_HELPER(warningName) "\"")
+    #define NC_DISABLE_WARNING(warningName) _Pragma("GCC diagnostic ignored \"" STR_HELPER(warningName) "\"")
 #else
+    #define NC_EXPORT
+    #define NC_NORET
     #define NC_WARNING_PUSH
     #define NC_WARNING_POP
     #define NC_DISABLE_WARNING(warningNumber)
