171b7dd07d727dccbe2ecc34cced00b07dcf6de0
neru <152752583+neeeruuu@users.noreply.github.com>
2024-05-03 21:15:31 -0300

    refactor: rewrite events

--- src/lib/nativecord/classes/events.h
+++ src/lib/nativecord/classes/events.h
@@ -1,17 +1,13 @@
 #pragma once
 
-#include "nativecord/util/assert.h"
-
-#include <functional>
 #include <memory>
-#include <string>
+#include <stdexcept>
 #include <unordered_map>
 #include <vector>
 
 /*
     to-do:
-        test for safety?
-        try to get as many compile-time asserts as possible
+        add way to unregister listeners
 */
 
 template <typename T> struct function_traits : public function_traits<decltype(&T::operator())>
@@ -21,17 +17,16 @@ template <typename T> struct function_traits : public function_traits<decltype(&
 template <typename ClassType, typename ReturnType, typename... Args>
 struct function_traits<ReturnType (ClassType::*)(Args...) const>
 {
-        using argTypes = std::tuple<Args...>;
+        using argsTuple = std::tuple<Args...>;
 };
 
 template <typename F> using function_info = function_traits<decltype(&F::operator())>;
 
-// not using a pair but using this struct instead because i plan on storing more info
 struct EventInfo
 {
     public:
         std::vector<size_t> _types;
-        std::vector<void*> _functions;
+        std::vector<void*> _listeners;
 };
 
 class EventEmitter
@@ -41,6 +36,7 @@ class EventEmitter
         {
             return {typeid(T).hash_code()...};
         }
+
         template <typename Tuple, size_t Index = 0> void getTupleTypeHashCodes(std::vector<size_t>* hashList)
         {
             if constexpr (Index < std::tuple_size_v<Tuple>)
@@ -54,47 +50,47 @@ class EventEmitter
         template <typename... Args> inline void registerEvent(std::string eventName)
         {
             std::vector<size_t> types = getTypeHashCodes<Args...>();
-
             auto info = std::make_unique<EventInfo>();
             info->_types = types;
-
             _events.insert(std::make_pair(eventName, std::move(info)));
         }
 
         template <typename... Args> inline void fireEvent(std::string eventName, Args&&... args)
         {
-            ASSERT(_events.contains(eventName), "attempted to fire non-existing event");
+            if (!_events.contains(eventName))
+                throw new std::logic_error("attempted to fire non-existing event");
+
             auto it = _events.find(eventName);
-            ASSERT(it != _events.end(), "couldn't find eventinfo");
             std::unique_ptr<EventInfo>& eventInfo = it->second;
+            std::vector<size_t> argTypes = getTypeHashCodes<Args...>();
 
-            std::vector<size_t> types = getTypeHashCodes<Args...>();
-            ASSERT(eventInfo->_types == types, "type mismatch during event fire");
+            if (argTypes != eventInfo->_types)
+                throw new std::logic_error("event args do not match args passed to fireEvent");
 
-            using FuncType = std::function<void(Args...)>;
-            for (auto funcPtr : eventInfo->_functions)
+            using FunctionType = std::function<void(Args...)>;
+            for (auto functionPointer : eventInfo->_listeners)
             {
-                auto fn = reinterpret_cast<void (*)(Args...)>(funcPtr);
+                auto fn = reinterpret_cast<void (*)(Args...)>(functionPointer);
                 fn(std::forward<Args>(args)...);
             }
         }
 
-        template <typename Func> inline void on(std::string eventName, Func func)
+        template <typename F> inline void on(std::string eventName, F listenerFn)
         {
-            ASSERT(_events.contains(eventName), "attempted to register callback for non-existing event");
+            if (!_events.contains(eventName))
+                throw new std::logic_error("attempted to register listener for non-existeng event");
+
             auto it = _events.find(eventName);
-            ASSERT(it != _events.end(), "couldn't find event");
+            std::unique_ptr<EventInfo>& eventInfo = it->second;
 
-            std::unique_ptr<EventInfo>& event = it->second;
+            using FI = function_info<decltype(listenerFn)>;
+            std::vector<size_t> listenerArgHashes{};
+            getTupleTypeHashCodes<typename FI::argsTuple>(&listenerArgHashes);
 
-            using FI = function_info<decltype(func)>;
-            std::vector<size_t> funcTypeHashes{};
-            getTupleTypeHashCodes<typename FI::argTypes>(&funcTypeHashes);
+            if (eventInfo->_types != listenerArgHashes)
+                throw new std::logic_error("listener function signature does not match event");
 
-            ASSERT(event->_types.size() == funcTypeHashes.size(), "argument count did not match");
-            ASSERT(funcTypeHashes == event->_types, "func type does not match event type");
-
-            event->_functions.push_back(func);
+            eventInfo->_listeners.push_back(listenerFn);
         }
 
     private:
