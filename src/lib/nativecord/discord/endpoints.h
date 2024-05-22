7b4a310e105c600e8e75770ede84adfaf80be4ea
neru <152752583+neeeruuu@users.noreply.github.com>
2024-05-22 05:55:30 -0300

    style: rename endpoint definitions

--- src/lib/nativecord/discord/endpoints.h
+++ src/lib/nativecord/discord/endpoints.h
@@ -2,8 +2,9 @@
 
 #include "nativecord/util/macros.h"
 
-#define NC_DISCORD "https://discord.com"
+#define DISCORD_URL "https://discord.com/"
 
-#define NC_API_VERSION 9
-#define NC_API_URL NC_DISCORD "/api/v" STR(NC_API_VERSION) "/"
-#define NC_WSS_URL "wss://gateway.discord.gg/?encoding=json&v=" STR(NC_API_VERSION)
+#define DISCORD_API_VERSION 9
+#define DISCORD_API_URL DISCORD_URL "api/v" STR(DISCORD_API_VERSION) "/"
+
+#define DISCORD_WS_URL "wss://gateway.discord.gg/?encoding=json&v=" STR(DISCORD_API_VERSION)
